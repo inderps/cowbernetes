5cdd203e1f6a1395da2a4a0f1f664c5acc985c01
Inderpal Singh <singhinther@gmail.com>
2019-03-07 21:07:31 -0500

    8bit

--- src/client/components/SheepFarm/index.js
+++ src/client/components/SheepFarm/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Pod from '../Pod';
 import './SheepFarm.css';
+import houseImage from './assets/house.png';
 
 const SheepFarm = ({ controller }) => (
   <div className="sheep-farm">
     <div className="billboard">{controller.name}</div>
+    <img src={houseImage} className="house" alt="house" />
     <div className="farm">
       {
         controller.pods.map(pod => (<Pod key={pod} pod={pod} />))
