88d7b6af0dbe7d2546b348dd5f5a6dee340417d0
Inderpal Singh <singhinther@gmail.com>
2019-03-12 20:24:50 -0400

    new billboard

--- src/client/components/Cow/index.js
+++ src/client/components/Cow/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Cow.css';
-import cow from './assets/cow.gif';
+import fineCow from './assets/cow.gif';
 import deadCow from './assets/dead-cow.png';
 import sickCow from './assets/sick-cow.gif';
 // https://www.flaticon.com/free-icon/cow_194983
 // import Cow from 'svg-react-loader?name=Cow!./assets/cow.svg';
 
-const Cow = () => (<img className="cow" src={cow} alt='cow' />);
+const Cow = ({ pod }) => {
+  let cow = deadCow, cowClass = 'cow shaking'
+
+  switch(pod.status) {
+    case 'Running':
+      cow = sickCow;
+      break;
+    case 'ContainerCreating':
+      cowClass = `${cowClass} shaking`;
+      break;
+    case 'Terminating':
+      cow = deadCow;
+      cowClass = `${cowClass} shaking`;
+      break;
+  }
+
+  console.log(pod)
+  return (<img className={cowClass} src={cow} alt='cow' />);
+};
 
 export default Cow;
